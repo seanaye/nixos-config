85428659af1fee8127e049fc61f56c269c781d45
Sean Aye <hello@seanaye.ca>
2025-08-31 11:57:17 -0400

    update home assistant

--- hosts/mira/home.nix
+++ hosts/mira/home.nix
@@ -5,6 +5,18 @@
     ../common/home.nix
   ];
 
-  services.home-assistant.enable = true;
+  services.home-assistant = {
+    enable = true;
+    extraComponents = [
+      "esphome"
+      "met"
+      "radio_browser"
+    ];
+    config = {
+      default_config = { };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8132 ];
 
 }
